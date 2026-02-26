80e3a8f3ef24c35bce7f39b7ac169c41dcb56497
dykim <royalkdy@gmail.com>
2026-02-26 19:27:35 +0900

    config import수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ export default [
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
       },
       globals: {
